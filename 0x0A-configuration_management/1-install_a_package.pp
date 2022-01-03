7062e879cec15ebe58030af89bc4edf304dad11a
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-03 19:54:03 +0000

    Added source

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # Install puppet-lint 2.5.0
 
 package { 'puppet-lint':
-    ensure   => '2.5.0',
-    provider => 'gem',
+  ensure   => '2.5.0',
+  provider => 'gem',
+  source   => 'https://rubygems.org/',
 }
