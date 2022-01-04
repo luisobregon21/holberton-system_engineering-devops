c235f5673532164a7fb9f3c62c2e581aea60ebd2
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-04 18:30:20 +0000

    Add new task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # sets up your client SSH configuration file so that you can connect to a server without typing a password.
 
 include stdlib
-file_line{'change private key':
+file_line{ 'change private key':
   ensure => present,
   path   => '~/etc/ssh/ssh_config',
   line   => 'IdentifyFile ~/.ssh/school'
 }
-file_line {'no password':
+file_line { 'no password':
   ensure => present,
   path   => '~/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
