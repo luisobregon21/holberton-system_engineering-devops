8b788e68f53be04548efdcf36e63b78691369492
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-31 20:13:32 +0000

    Back to my code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-"""
-module that takes argv 1 and uses it to request to a fake api
-"""
+''' returns information about his/her TODO list progress '''
+
 import requests
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(argv) == 2 and argv[1].isdigit():
-        num = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
+        url = 'https://jsonplaceholder.typicode.com/'
         usr = requests.get(url+'users/{}'.format(argv[1])).json()["name"]
-        r = requests.get(url + "todos?userId={}".format(num)).json()
-        tasks = 0
-        total = 0
-        nametask = ""
-        for element in r:
-            if element["completed"] is True:
-                tasks += 1
-                nametask += "\t " + element["title"] + "\n"
-            total += 1
-        print("Exployee {} is done with tasks({}/{}):".format(usr,
-                                                              tasks, total))
-        print(nametask, end="")
-    else:
-        print("length WRONG")
+        request = requests.get(url+'todos?userId={}'.format(argv[1]))
+        dic = request.json()
+        num = 0
+        taskname = ""
+        for task in dic:
+            if task['completed'] is True:
+                num += 1
+                taskname += "\t " + task['title'] + '\n'
+        print("Employee {} is done with tasks({}/20):".format(usr, num))
+        print(taskname, end="")
