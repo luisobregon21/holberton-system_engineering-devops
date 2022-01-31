35e1a9bf625004ef1d33417384da4763bd0c977f
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-31 19:43:01 +0000

    Add new task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' returns information about his/her TODO list progress '''
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) == 2 and argv[1].isdigit():
+        url = 'https://jsonplaceholder.typicode.com/'
+        usr = requests.get(url+'users/{}'.format(argv[1])).json()["name"]
+        request = requests.get(url+'todos?userId={}'.format(argv[1]))
+        dic = request.json()
+        num = 0
+        taskname = ""
+        for task in dic:
+            if task['completed'] is True:
+                num += 1
+                taskname += "\t" + task['title'] + '\n'
+        print("Employee {} is done with tasks:({}/20)".format(usr, num))
+        print(taskname, end="")
