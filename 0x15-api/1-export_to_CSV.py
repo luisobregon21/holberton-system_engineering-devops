750ff73e0978f11d80eac1720d427021b1086486
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-31 20:55:34 +0000

    add new task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' returns information about his/her TODO list progress '''
+
+import os
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    if len(argv) == 2 and argv[1].isdigit():
+        url = 'https://jsonplaceholder.typicode.com/'
+        usr = requests.get(url+'users/{}'.format(argv[1])).json()["username"]
+        request = requests.get(url+'todos?userId={}'.format(argv[1]))
+        dic = request.json()
+        num = 0
+        taskname = ""
+        with open ("USER_ID.csv", 'w', encoding='utf-8') as csv:
+            for task in dic:
+                csv.write('"{}","{}","{}","{}"\n'.format(argv[1], usr, task["completed"], task['title']))
+            csv.close()
